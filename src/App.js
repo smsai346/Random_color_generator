241dbe94583c378ae0ab3a4f7ddacffccef47d63
smsai346 <smsai4u@gmail.com>
2024-02-26 22:34:00 +0530

    random color generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [typeOfColor,setTypeOfColor]=useState("hex")
+const [color,setColor]=useState(['#000000'])
+const handleCreateRandomhexColor=()=>{
+  const hexa=[1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F']
+  let hexcode='#';
+  for(let i=0;i<6;i++){
+    hexcode +=hexa[Math.floor(Math.random()*hexa.length)]
+  }
+  console.log(hexcode)
+  setColor([hexcode])
+}
+const handleCreateRandomrgbColor=()=>{
+  const r=Math.floor(Math.random()*256)
+  const g=Math.floor(Math.random()*256)
+  const b=Math.floor(Math.random()*256)
+  console.log(`rgb${r},${g},${b}`)
+  setColor(`rgb(${r},${g},${b})`)
+
+}
+useEffect(()=>{
+   if (typeOfColor==="hex") handleCreateRandomhexColor();
+  else if (typeOfColor==="rgb") handleCreateRandomrgbColor();},[typeOfColor]
+)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="back" style={{backgroundColor:`${color}`}}>
+      <div className="d">
+    <button onClick={()=>setTypeOfColor('hex')} className="button" >Create HEX Color</button>
+    <button onClick={()=>setTypeOfColor('rgb')} className="button" >Create RGB Color</button>
+    <button onClick={typeOfColor==='hex'?(handleCreateRandomhexColor):(handleCreateRandomrgbColor)} className="button" >Generate Random Color</button>
     </div>
+    <div className="textdes">
+    <h1>{typeOfColor==="hex"?"HEX":"RGB"}</h1>
+    <h1>{color}</h1>
+    </div>
+</div>
   );
 }
 
